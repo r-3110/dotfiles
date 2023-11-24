01b8ca52e264af28b3e2a9cdcd75645d93647c15
r-3110 <ryo3110yamanashi@gmail.com>
2023-11-24 13:08:12 +0900

    starshipに移行

--- .oh-my-bash/completions/awscli.completion.sh
+++
@@ -1,2 +0,0 @@
-#! bash oh-my-bash.module
-[[ -x "$(which aws_completer)" ]] &>/dev/null && complete -C "$(which aws_completer)" aws
--- .oh-my-bash/completions/kontena.completion.sh
+++
@@ -1,2 +0,0 @@
-#! bash oh-my-bash.module
-which kontena &> /dev/null && . "$( kontena whoami --bash-completion-path )"
