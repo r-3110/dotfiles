b18b685279b730fe94fac7c3352950cd1b374fb4
Ryo <ryo3110yamanashi@gmail.com>
2024-11-23 14:39:35 +0900

    wezterm keybind追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,4 +18,69 @@ config.font_size = 18.0
 config.font = wezterm.font_with_fallback { "MesloLGS NF" }
 
 -- and finally, return the configuration to wezterm
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+
+local act = wezterm.action
+
+config.key_tables = {
+	copy_mode = {
+		-- 移動
+		{ key = "h", mods = "NONE", action = act.CopyMode("MoveLeft") },
+		{ key = "j", mods = "NONE", action = act.CopyMode("MoveDown") },
+		{ key = "k", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "l", mods = "NONE", action = act.CopyMode("MoveRight") },
+		-- 最初と最後に移動
+		{ key = "^", mods = "NONE", action = act.CopyMode("MoveToStartOfLineContent") },
+		{ key = "$", mods = "NONE", action = act.CopyMode("MoveToEndOfLineContent") },
+		-- 左端に移動
+		{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+		{ key = "o", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEnd") },
+		{ key = "O", mods = "NONE", action = act.CopyMode("MoveToSelectionOtherEndHoriz") },
+		--
+		{ key = ";", mods = "NONE", action = act.CopyMode("JumpAgain") },
+		-- 単語ごと移動
+		{ key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+		{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "e", mods = "NONE", action = act.CopyMode("MoveForwardWordEnd") },
+		-- ジャンプ機能 t f
+		{ key = "t", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = true } }) },
+		{ key = "f", mods = "NONE", action = act.CopyMode({ JumpForward = { prev_char = false } }) },
+		{ key = "T", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = true } }) },
+		{ key = "F", mods = "NONE", action = act.CopyMode({ JumpBackward = { prev_char = false } }) },
+		-- 一番下へ
+		{ key = "G", mods = "NONE", action = act.CopyMode("MoveToScrollbackBottom") },
+		-- 一番上へ
+		{ key = "g", mods = "NONE", action = act.CopyMode("MoveToScrollbackTop") },
+		-- viweport
+		{ key = "H", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
+		{ key = "L", mods = "NONE", action = act.CopyMode("MoveToViewportBottom") },
+		{ key = "M", mods = "NONE", action = act.CopyMode("MoveToViewportMiddle") },
+		-- スクロール
+		{ key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
+		{ key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
+		{ key = "d", mods = "CTRL", action = act.CopyMode({ MoveByPage = 0.5 }) },
+		{ key = "u", mods = "CTRL", action = act.CopyMode({ MoveByPage = -0.5 }) },
+		-- 範囲選択モード
+		{ key = "v", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Cell" }) },
+		{ key = "v", mods = "CTRL", action = act.CopyMode({ SetSelectionMode = "Block" }) },
+		{ key = "V", mods = "NONE", action = act.CopyMode({ SetSelectionMode = "Line" }) },
+		-- コピー
+		{ key = "y", mods = "NONE", action = act.CopyTo("Clipboard") },
+
+		-- コピーモードを終了
+		{
+		key = "Enter",
+		mods = "NONE",
+		action = act.Multiple({ { CopyTo = "ClipboardAndPrimarySelection" }, { CopyMode = "Close" } }),
+		},
+		{ key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
+		{ key = "c", mods = "CTRL", action = act.CopyMode("Close") },
+		{ key = "q", mods = "NONE", action = act.CopyMode("Close") },
+	},
+}
+
+
 return config
