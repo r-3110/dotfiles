b5e25dc4817fec7b15631309983f26a7aef7ef28
Ryo <ryo3110yamanashi@gmail.com>
2024-11-29 16:12:37 +0900

    cfn-lint設定

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,47 +1,107 @@
-if vim.g.vscode then
-	local function notVSCode()
-		print("This plugin is not supported in VSCode")
+-- @see https://github.com/nvimtools/none-ls.nvim
+-- @see https://github.com/jose-elias-alvarez/null-ls.nvim
+-- @see https://github.com/williamboman/mason.nvim
+
+-- テーブルをダンプする関数
+-- @param t ダンプ対象のテーブル
+-- @param indent インデントレベル（再帰呼び出し用）
+local function dumpTable(t, indent)
+	indent = indent or 0
+	local prefix = string.rep("  ", indent) -- インデント用のスペース
+	if type(t) ~= "table" then
+		return tostring(t) -- テーブル以外はそのまま返す
 	end
 
-	return notVSCode
+	local result = "{\n"
+	for key, value in pairs(t) do
+		-- キーを文字列に変換
+		local keyStr = tostring(key)
+		if type(key) == "string" then
+			keyStr = string.format("%q", key) -- 文字列キーを引用符で囲む
+		end
+
+		-- 値を適切にダンプ
+		local valueStr
+		if type(value) == "table" then
+			valueStr = dumpTable(value, indent + 1) -- 再帰呼び出し
+		else
+			valueStr = tostring(value)
+			if type(value) == "string" then
+				valueStr = string.format("%q", value) -- 文字列値を引用符で囲む
+			end
+		end
+
+		result = result .. prefix .. "  [" .. keyStr .. "] = " .. valueStr .. ",\n"
+	end
+	result = result .. prefix .. "}"
+	return result
 end
 
 return {
-	-- add tsserver and setup with typescript.nvim instead of lspconfig
-	-- {
-	-- 	"neovim/nvim-lspconfig",
-	-- 	dependencies = {
-	-- 		"jose-elias-alvarez/typescript.nvim",
-	-- 		init = function()
-	-- 			require("lazyvim.util").lsp.on_attach(function(_, buffer)
-	--       -- stylua: ignore
-	--       vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-	-- 				vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-	-- 			end)
-	-- 		end,
-	-- 	},
-	-- 	---@class PluginLspOpts
-	-- 	opts = {
-	-- 		---@type lspconfig.options
-	-- 		servers = {
-	-- 			-- tsserver will be automatically installed with mason and loaded with lspconfig
-	-- 			tsserver = {},
-	-- 		},
-	-- 		-- you can do any additional lsp server setup here
-	-- 		-- return true if you don't want this server to be setup with lspconfig
-	-- 		---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-	-- 		setup = {
-	-- 			-- example to setup with typescript.nvim
-	-- 			tsserver = function(_, opts)
-	-- 				require("typescript").setup({ server = opts })
-	-- 				return true
-	-- 			end,
-	-- 			-- Specify * to use this function as a fallback for any server
-	-- 			-- ["*"] = function(server, opts) end,
-	-- 		},
-	-- 	},
-	-- },
+	{ import = "lazyvim.plugins.extras.lsp.none-ls" },
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = function(_, opts)
+			local nls = require("null-ls")
 
+			nls.setup({
+				debug = false,
+				sources = {
+					nls.builtins.formatting.fish_indent,
+					nls.builtins.diagnostics.fish,
+					nls.builtins.formatting.stylua,
+					nls.builtins.formatting.shfmt,
+					nls.builtins.diagnostics.cfn_lint.with({
+						-- @see https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+						args = { "--format", "parseable", "$FILENAME" },
+					}),
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				yamlls = {
+					settings = {
+						yaml = {
+							customTags = {
+								"!And",
+								"!And sequence",
+								"!If",
+								"!If sequence",
+								"!Not",
+								"!Not sequence",
+								"!Equals",
+								"!Equals sequence",
+								"!Or",
+								"!Or sequence",
+								"!FindInMap",
+								"!FindInMap sequence",
+								"!Base64",
+								"!Join",
+								"!Join sequence",
+								"!Cidr",
+								"!Ref",
+								"!Sub",
+								"!Sub sequence",
+								"!GetAtt",
+								"!GetAZs",
+								"!ImportValue",
+								"!ImportValue sequence",
+								"!Select",
+								"!Select sequence",
+								"!Split",
+								"!Split sequence",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 	-- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
 	-- treesitter, mason and typescript.nvim. So instead of the above, you can use:
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
