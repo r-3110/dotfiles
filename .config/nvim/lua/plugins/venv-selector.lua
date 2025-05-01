9b23544da9c7ac8cf0b2ea4038ab21c7cbe51021
Ryo <ryo3110yamanashi@gmail.com>
2025-05-01 10:02:57 +0900

    venv-selector追加

--- .config/nvim/lua/plugins/venv-selector.lua
+++ .config/nvim/lua/plugins/venv-selector.lua
@@ -1,18 +1,18 @@
 --@see https://github.com/linux-cultist/venv-selector.nvim/tree/regexp
 
 return {
-  "linux-cultist/venv-selector.nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    -- "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
-    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-  },
-  lazy = false,
-  branch = "regexp", -- This is the regexp branch, use this for the new version
-  ---@type venv-selector.Config
-  opts = {
-    -- Your settings go here
-  },
-  { "mfussenegger/nvim-dap", enabled = false },
-  { "mfussenegger/nvim-dap-python", enabled = false },
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		-- "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	---@type venv-selector.Config
+	opts = {
+		-- Your settings go here
+	},
+	{ "mfussenegger/nvim-dap", enabled = false },
+	{ "mfussenegger/nvim-dap-python", enabled = false },
 }
