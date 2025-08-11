906eb17298c92f9ca5d613605a2930fc693aa0f6
Ryo <ryo3110yamanashi@gmail.com>
2025-08-11 17:16:20 +0900

    snippet追加

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -13,6 +13,7 @@ return {
 			build = "make install_jsregexp",
 			config = function()
 				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_lua").load({ paths = { "~/dotfiles/.config/nvim/snippet" } })
 			end,
 		},
 		-- ... Other dependencies
