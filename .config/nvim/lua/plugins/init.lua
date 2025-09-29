cc3e16617d99baa92042d02a99f96233237fab49
Ryo <ryo3110yamanashi@gmail.com>
2025-09-29 10:14:01 +0900

    nvim lualine設定変更

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -13,24 +13,67 @@ return {
 	---@type LazyPluginSpec
 	{
 		"nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 		event = "VeryLazy",
 		opts = function(_, opts)
-			table.insert(opts.sections.lualine_x, {
-				function()
-					return "😄"
-				end,
-			})
+      local icons = LazyVim.config.icons
+
+      opts.sections.lualine_x = {
+        Snacks.profiler.status(),
+         -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
+        },
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = function() return { fg = Snacks.util.color("Constant") } end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return { fg = Snacks.util.color("Debug") } end,
+        },
+        -- stylua: ignore
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = function() return { fg = Snacks.util.color("Special") } end,
+        },
+        {
+          "diff",
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            removed = icons.git.removed,
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+        "encoding",
+        "fileformat",
+        "filetype"
+      }
+
+      opts.sections.lualine_z = {
+        function()
+          return " " .. os.date("%R")
+        end,
+      }
+
+      return opts
 		end,
 	},
-
-	-- or you can return new options to override all the defaults
-	-- {
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	event = "VeryLazy",
-	-- 	opts = function()
-	-- 		return {
-	-- 			--[[add your custom lualine config here]]
-	-- 		}
-	-- 	end,
-	-- },
 }
