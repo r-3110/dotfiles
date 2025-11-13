58d265debcfbf828d862df11378b0241d09458d6
Ryo <ryo3110yamanashi@gmail.com>
2025-11-13 21:24:44 +0900

    temp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -2,140 +2,172 @@
 -- @see https://github.com/bassamsdata/namu.nvim
 -- @see https://github.com/dnlhc/glance.nvim
 
-vim.lsp.enable({ "pyright" })
-
 -- vtslsの再起動コマンド monorepoだと最初にアタッチされたtsconfigを参照し続けるため
 vim.keymap.set("n", "<leader>tr", ":LspRestart vtsls<CR>", { desc = "vtsls Restart" })
 
 ---@type LazyPluginSpec[]
 return {
 	{
-		"neovim/nvim-lspconfig",
+		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
 			{
-				"mason-org/mason-lspconfig.nvim",
-				version = "^2.0.0",
-				dependencies = {
-					{
-						"mason-org/mason.nvim",
-						---@module "mason"
-						---@type MasonSettings
-						opts = {},
-					},
-				},
-				---@module "mason-lspconfig"
-				---@type MasonLspconfigSettings
-				opts = {
-					ensure_installed = {
-						"bashls",
-						"biome",
-						"copilot",
-						"denols",
-						"docker_compose_language_service",
-						"dockerls",
-						"eslint",
-						"gh_actions_ls",
-						"gopls",
-						"html",
-						"jsonls",
-						"jsonnet_ls",
-						"lua_ls",
-						"marksman",
-						"phpactor",
-						"pyright",
-						"ruff",
-						"stylua",
-						"taplo",
-						"tsp_server",
-						"vtsls",
-						"yamlls",
-					},
-				},
+				"mason-org/mason.nvim",
+				---@module "mason"
+				---@type MasonSettings
+				opts = {},
 			},
 			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				dependencies = {
-					{ "DrKJeff16/wezterm-types", lazy = true },
-				},
-				---@module "lazydev"
-				---@type lazydev.Config
-				opts = {
-					debug = false,
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{
-							path = {
-								"${3rd}/luv/library",
-							},
-							words = { "vim%.uv" },
+				"neovim/nvim-lspconfig",
+				---@module "lspconfig"
+				---@param opts PluginLspOpts
+				opts = function(_, opts)
+					local root_pattern = require("lspconfig.util").root_pattern
+
+					---@type table<string, lazyvim.lsp.Config|boolean>
+					local manual_setup_lsp = {
+						denols = {
+							mason = false,
+							root_dir = function(bufnr, on_dir)
+								local fname = vim.api.nvim_buf_get_name(bufnr)
+								local filepath = root_pattern("deno.json", "deno.jsonc")(fname)
+
+								if filepath == nil then
+									return
+								end
+
+								on_dir(root_pattern("deno.json", "deno.jsonc")(fname))
+							end,
+							-- root_markers = { "deno.json", "deno.jsonc" },
 						},
-						"lazy.nvim",
-						"LazyVim",
-						{ path = "LazyVim", words = { "LazyVim" } },
-						{ path = "wezterm-types", mods = { "wezterm" } },
-					},
-					-- luarc.jsonによるlibraryの読み込みだとlsp attach時に読み込みが巨大になってしまう。
-					-- nvimではlazyvimで必要な時に必要なファイルだけ読むように。
-					-- vscodeではあまり編集しないので妥協として.settings.jsonで有効化することで対応している。
-					---@type boolean|(fun(root:string):boolean?)
-					enabled = true,
-				},
+						vtsls = {
+							mason = false,
+							root_dir = function(bufnr, on_dir)
+								local fname = vim.api.nvim_buf_get_name(bufnr)
+
+								local deno_filepath = root_pattern("deno.json", "deno.jsonc")(fname)
+
+								-- deno.jsonがある場合は起動しない
+								if deno_filepath ~= nil then
+									return
+								end
+
+								on_dir(root_pattern("tsconfig.json", "package.json")(fname))
+							end,
+							-- root_markers = { "tsconfig.json", "package.json" },
+						},
+						yamlls = {
+							filetypes = { "yaml", "yml" },
+							settings = {
+								yaml = {
+									schemas = {
+										["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+									},
+									customTags = {
+										"!And",
+										"!And sequence",
+										"!If",
+										"!If sequence",
+										"!Not",
+										"!Not sequence",
+										"!Equals",
+										"!Equals sequence",
+										"!Or",
+										"!Or sequence",
+										"!FindInMap",
+										"!FindInMap sequence",
+										"!Base64",
+										"!Join",
+										"!Join sequence",
+										"!Cidr",
+										"!Ref",
+										"!Sub",
+										"!Sub sequence",
+										"!GetAtt",
+										"!GetAZs",
+										"!ImportValue",
+										"!ImportValue sequence",
+										"!Select",
+										"!Select sequence",
+										"!Split",
+										"!Split sequence",
+									},
+								},
+							},
+						},
+					}
+
+					local merged_config = vim.tbl_deep_extend("force", {}, opts.servers or {}, manual_setup_lsp or {})
+
+					-- 下記のようにlazyvimではlspの設定が自動化されているため、必要に応じてここで上書きする。
+					--@see https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/lsp/init.lua
+					opts.servers = merged_config
+
+					return opts
+				end,
 			},
 		},
+		config = function()
+			---@module "mason-lspconfig"
+			local mason_lspconfig = require("mason-lspconfig")
+
+			mason_lspconfig.setup({
+				automatic_enable = false,
+				ensure_installed = {
+					"bashls",
+					"biome",
+					"copilot",
+					"denols",
+					"docker_compose_language_service",
+					"dockerls",
+					"eslint",
+					"gh_actions_ls",
+					"gopls",
+					"html",
+					"jsonls",
+					"jsonnet_ls",
+					"lua_ls",
+					"marksman",
+					"phpactor",
+					"pyright",
+					"ruff",
+					"stylua",
+					"taplo",
+					"tsp_server",
+					"vtsls",
+					"yamlls",
+				},
+			})
+		end,
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		dependencies = {
+			{ "DrKJeff16/wezterm-types", lazy = true },
+		},
+		---@module "lazydev"
+		---@type lazydev.Config
 		opts = {
-			servers = {
-				--- typescript-toolsを使用するため無効に
-				tsserver = {
-					enabled = false,
-				},
-				ts_ls = {
-					enabled = false,
-				},
-				vtsls = {
-					enabled = true,
-				},
-				yamlls = {
-					filetypes = { "yaml", "yml" },
-					settings = {
-						yaml = {
-							schemas = {
-								["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-							},
-							customTags = {
-								"!And",
-								"!And sequence",
-								"!If",
-								"!If sequence",
-								"!Not",
-								"!Not sequence",
-								"!Equals",
-								"!Equals sequence",
-								"!Or",
-								"!Or sequence",
-								"!FindInMap",
-								"!FindInMap sequence",
-								"!Base64",
-								"!Join",
-								"!Join sequence",
-								"!Cidr",
-								"!Ref",
-								"!Sub",
-								"!Sub sequence",
-								"!GetAtt",
-								"!GetAZs",
-								"!ImportValue",
-								"!ImportValue sequence",
-								"!Select",
-								"!Select sequence",
-								"!Split",
-								"!Split sequence",
-							},
-						},
+			debug = false,
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{
+					path = {
+						"${3rd}/luv/library",
 					},
+					words = { "vim%.uv" },
 				},
+				"lazy.nvim",
+				"LazyVim",
+				{ path = "LazyVim", words = { "LazyVim" } },
+				{ path = "wezterm-types", mods = { "wezterm" } },
 			},
+			-- luarc.jsonによるlibraryの読み込みだとlsp attach時に読み込みが巨大になってしまう。
+			-- nvimではlazyvimで必要な時に必要なファイルだけ読むように。
+			-- vscodeではあまり編集しないので妥協として.settings.jsonで有効化することで対応している。
+			---@type boolean|(fun(root:string):boolean?)
+			enabled = true,
 		},
 	},
 	{
