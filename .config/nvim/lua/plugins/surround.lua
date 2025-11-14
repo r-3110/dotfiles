1394df911879f50f0e391b375e037061cdd67c53
Ryo <ryo3110yamanashi@gmail.com>
2025-11-14 21:07:21 +0900

    nvim keymap変更

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -10,6 +10,19 @@ return {
 		---@module "nvim-surround"
 		require("nvim-surround").setup({
 			-- Configuration here, or leave empty to use defaults
+			keymaps = {
+				insert = "<C-g>s",
+				insert_line = "<C-g>S",
+				normal = "ys",
+				normal_cur = "yss",
+				normal_line = "yS",
+				normal_cur_line = "ySS",
+				visual = "gs",
+				visual_line = "gS",
+				delete = "ds",
+				change = "cs",
+				change_line = "cS",
+			},
 		})
 	end,
 }
