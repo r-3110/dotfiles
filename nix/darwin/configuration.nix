42f125665c755c8703902faa0f9b05dbdead3932
Ryo <ryo3110yamanashi@gmail.com>
2026-02-16 13:55:55 +0900

    add nix pkgs

--- nix/darwin/configuration.nix
+++ nix/darwin/configuration.nix
@@ -56,6 +56,9 @@
       "clawscli/tap/claws"
       "slack"
       "ghostty"
+      "microsoft-word"
+      "microsoft-excel"
+      "microsoft-powerpoint"
     ];
     # https://apps.apple.com/jp
     masApps = {
