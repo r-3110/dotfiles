f7caebb7ac617cb234df93193c01e714355cf746
Ryo <ryo3110yamanashi@gmail.com>
2026-04-05 14:20:27 +0900

    nvim motion系plugin整理

--- .config/nvim/lua/plugins/accelerated-jk.lua
+++
@@ -1,7 +0,0 @@
--- @see https://github.com/rainbowhxch/accelerated-jk.nvim
-
----@type LazyPluginSpec
-return {
-	"rainbowhxch/accelerated-jk.nvim",
-	vscode = false,
-}
--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -1,5 +1,9 @@
 --@see https://github.com/nekowasabi/hellshake-yano.vim
+--@see https://github.com/rainbowhxch/accelerated-jk.nvim
+--@see https://github.com/folke/flash.nvim
+--@see https://github.com/s-show/extend_word_motion.nvim
 
+---@module "lazy"
 ---@type LazyPluginSpec[]
 return {
 	{
@@ -18,5 +22,34 @@ return {
 				enableHighlight = true,
 			}
 		end,
+	},
+	{
+		"rainbowhxch/accelerated-jk.nvim",
+		vscode = false,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		lazy = true,
+		---@type Flash.Config
+		---@diagnostic disable-next-line: missing-fields
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		},
+	},
+	{
+		"s-show/extend_word_motion.nvim",
+		event = "VeryLazy",
+		lazy = true,
+		opts = {},
+		dependencies = {
+			"sirasagi62/tinysegmenter.nvim",
+		},
 	},
 }
