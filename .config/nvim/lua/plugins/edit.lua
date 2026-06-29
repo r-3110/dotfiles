2c386b7d5048090cc996ca3b37e1d47e9f8eb59c
Ryo <ryo3110yamanashi@gmail.com>
2026-06-29 10:45:01 +0900

    feat(config): nvim autopairs config

--- .config/nvim/lua/plugins/edit.lua
+++ .config/nvim/lua/plugins/edit.lua
@@ -1,21 +1,44 @@
---@see https://github.com/qwavies/smart-backspace.nvim
 --@see https://github.com/windwp/nvim-autopairs
+--@see https://github.com/kylechui/nvim-surround
 
 ---@type LazyPluginSpec[]
 return {
 	{
-		"qwavies/smart-backspace.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		opts = {
-			silent = false,
-		},
+		"nvim-mini/mini.pairs",
+		enabled = false,
 	},
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
 		opts = {
-			map_bs = false,
+			map_bs = true,
+			check_ts = true,
 		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		lazy = true,
+		config = function()
+			---@module "nvim-surround"
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+				keymaps = {
+					insert = "<C-g>s",
+					insert_line = "<C-g>S",
+					normal = "ys",
+					normal_cur = "yss",
+					normal_line = "yS",
+					normal_cur_line = "ySS",
+					visual = "gs",
+					visual_line = "gS",
+					delete = "ds",
+					change = "cs",
+					change_line = "cS",
+				},
+			})
+		end,
 	},
 }
